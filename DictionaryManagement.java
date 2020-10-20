2df5d1e4490728c139e77c40cf275431e1eede91
Viet Tran <tviet764@gmail.com>
2020-10-20 22:25:37 +0700

    Viet
    thay doi duong dan

--- DictionaryManagement.java
+++ DictionaryManagement.java
@@ -3,7 +3,6 @@ package sample;
 import java.io.*;
 
 public class DictionaryManagement extends Dictionary {
-
     private static final String DATA_FILE_PATH = "E_V.txt";
     private static final String SPLITTING_CHARACTERS = "<html>";
 
@@ -23,7 +22,7 @@ public class DictionaryManagement extends Dictionary {
         try {
             reader =
                     new BufferedReader(
-                            new FileReader("C:\\Users\\namtr\\IdeaProjects\\Dictionary\\src\\E_V.txt"));
+                            new FileReader("C:\\Users\\admin\\Downloads\\Dictionary\\Dictionary\\src\\E_V.txt"));
             String line = reader.readLine();
             while (line != null) {
                 String[] temp = line.split(SPLITTING_CHARACTERS);
